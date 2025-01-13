2e2827d838cbfa4d342bb5f1c94a5e32b820f69d
Jorge Valencia <jorge.valencia@DevSecOpsUsach.local>
2025-01-12 21:49:14 -0300

    Dependencias de Jest

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    collectCoverage: true,
+    coverageReporters: ['lcov', 'text'],
+    coverageDirectory: "coverage",
+    testMatch: ['**/*.spec.ts']
+  };
